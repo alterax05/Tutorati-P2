2e6517d1650cd4dd31af546e0e894a2d242c8eab
alterax05 <107863063+alterax05@users.noreply.github.com>
2026-03-26 18:12:18 +0100

    fix: fix tut3

--- tutorato 3 25-03/3TutP2/src/es1/MainEs1.java
+++ tutorato 3 25-03/3TutP2/src/es1/MainEs1.java
@@ -22,12 +22,5 @@ public class MainEs1 {
         System.out.println("--------------comapre--------------");
         System.out.println(abitanti[2].compare(abitanti[0]));
         System.out.println(abitanti[2].compare(abitanti[3]));
-
-        System.out.println("--------------isGenitore--------------");
-
-        // Il for ha un errore di, come lo potrete correggere?
-		/*for(Lander lander: abitanti){
-			System.out.println((abitanti[4]).isGenitore(lander));
-		}*/
     }
 }
--- tutorato 3 25-03/3TutP2/src/es1/flatland/Flatlander.java
+++ tutorato 3 25-03/3TutP2/src/es1/flatland/Flatlander.java
@@ -21,13 +21,4 @@ public class Flatlander extends Lander {
 		System.out.println("Sono un Flatlander " + this.genere +" quindi ho " + this.lati + " lati e " +
 				this.nDimensioni + " dimensioni");
 	}
-
-	// In generale si tende ad evitare l'utilizzo di instanceof all'interno dei metodi
-	// L'unica eccezione alla regola è il metodo equals
-	// In questo caso, come avremmo potuto modificare la firma del metodo per evitare l'uso di instance of?
-	public boolean isGenitore(Lander possibleParent){
-		return (this.genere == Genere.POLIGONO) &&
-				(possibleParent instanceof Flatlander) &&
-				(((Flatlander) possibleParent).lati == this.lati - 1);
-	}
 }
